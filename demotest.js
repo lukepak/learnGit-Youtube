79f7c07eb2fa595c02e2e17475b52002c13020d8
kiemnguyenduc <kiem.nguyenduc@gmail.com>
2022-04-10 21:36:58 +0700

    everthing merge

--- demotest.js
+++ demotest.js
@@ -1,7 +1,7 @@
 <<<<<<< HEAD
 git checkout master
 
-
+everthing ok
 
 Test git merge
 
@@ -12,4 +12,8 @@ s This is text file master
 This is text file brand2
 >>>>>>> second_brand
 
+This is file test ssh linux
 
+
+
+This is brand ssh second_brand
